2973baa346745dd2ea577135444973208d8d2c93
brooke <brooke0white@gmail.com>
2016-02-25 16:49:48 -0800

    Fixed issue with duplicate/non-numerical job numbers, updated ReadMe & Docstrings

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # HPLargeFormatPrinterFileOrganization
+A small script that organizes PDFs from an HP Large Printer
+(or any printer with filename of the form scanXXX.pdf) by the job
+number they are associated with.
+
+#Usage
+
+ex = OrganizePDFs(directory, beginning_file,
+                  job_list)
+ex.organize()
+
+directory = The directory in which the PDFs are found (ex. Users/brooke/Desktop/my_dir)
+
+beginning_file = The filename of the initial scanned file (ex. scan0001.pdf)
+
+job_list = A list of tuples of the form [("Job Number", Number of PDFs)]
+(ex. [("7190A", 1), ("7189&8120", 12), ("7128-T", 1), ("1234", 5)] )
--- main/python/main.py
+++ main/python/main.py
@@ -2,13 +2,31 @@ import os
 
 
 class OrganizePDFs:
-    def __init__(self, directory, beginning_file, job_dict):
+    """
+    Organizes archived engineering job PDFs in folders by their job number.
+    """
+    def __init__(self, directory, beginning_file, job_list):
+        """
+        Initializes Organize PDFs class
+        :param directory: The base directory in which unorganized PDFs are
+        stored.
+        :param beginning_file: The file name of the first scanXXX.pdf in the
+        base directory
+        :param job_list: A List of tuples of form
+        [("Job Number", int(Number of PDFs)]
+        """
         self.directory = directory
-        self.beginning_pdf_index = beginning_file[4:-4]
-        self.job_dict = job_dict
+        self.beginning_PDF_index = beginning_file[4:-4]
+        self.job_list = job_list
 
     def move_pdf_to_folder(self, job_number):
-        file_name = "scan"+str(self.beginning_pdf_index)+".pdf"
+        """
+        Moves a scanXXX.pdf file from the main directory to its job sub-folder.
+        :param job_number: A String containing the current job number
+        :return: None
+        """
+        file_name = "scan"+str(self.beginning_PDF_index) + ".pdf"
+        print(file_name)
         try:
             os.renames(
                 self.directory+file_name,
@@ -19,19 +37,37 @@ class OrganizePDFs:
             print("Error moving pdf: ", error.__str__())
 
     def increment(self, pdf_index):
-        self.beginning_pdf_index = '%05d' % (int(pdf_index) + 1)
+        """
+        Increments the current scanXXX.pdf by one.
+        :param pdf_index: The index of the current scanXXX.pdf
+        :return: None
+        """
+        self.beginning_PDF_index = '%05d' % (int(pdf_index) + 1)
 
     def path_is_valid(self, pdf_index):
-        return os.path.exists(self.directory+"scan"+str(pdf_index)+".pdf")
+        """
+        Determines if a given PDF file of the form scanXXX.pdf exists.
+        :param pdf_index: The index of the current scanXXX.pdf
+        :return: boolean: True if the scanXXX.pdf exists, False if does not
+        exist
+        """
+        return os.path.exists(
+            self.directory + "scan" + str(pdf_index) + ".pdf")
 
     def organize(self):
-
-        for job in self.job_dict:
+        """
+        Iterates through job_list creating folders for each job and moves PDFs.
+        :return: None
+        """
+        for job, PDFs in self.job_list:
             try:
                 sub_directory = self.directory+"#"+str(job)+"/"
 
+                # if directory has not been created already
                 if not os.path.isdir(sub_directory):
                     os.makedirs(sub_directory)
+                else:
+                    print("Dir already exists")
 
             except OSError as error:
                 print("error:\t"+error.__str__())
@@ -39,39 +75,27 @@ class OrganizePDFs:
             except Exception as error:
                 print("error:\t"+error.__str__())
                 return 2
-
-            for pdf in range(1, self.job_dict[job]+1):
-
-                if self.path_is_valid(self.beginning_pdf_index):
+            for x in range(0, PDFs):  # for every PDF per job file
+                if self.path_is_valid(self.beginning_PDF_index):
+                    print(job, str(PDFs))
                     self.move_pdf_to_folder(job_number=str(job))
                 else:
-                    while not self.path_is_valid(self.beginning_pdf_index):
-                        self.increment(self.beginning_pdf_index)
-                    self.move_pdf_to_folder(job_number=str(job))
+                    while not self.path_is_valid(self.beginning_PDF_index):
+                        self.increment(self.beginning_PDF_index)
+                        self.move_pdf_to_folder(job_number=str(job))
 
-                self.increment(self.beginning_pdf_index)
+                self.increment(self.beginning_PDF_index)
 
-# example usage
+# example usage #
+cur_job_list = [
+    ("06-66", 1), ("06-64", 1), ("06-63", 1), ("06-54", 1), ("06-50", 1),
+    ("06-46", 1), ("06-45", 1), ("06-39", 1), ("06-27", 1), ("06-23", 1),
+    ("06-22", 1), ("06-19", 1), ("06-12", 1), ("06-10", 1), ("06-09", 1),
+    ("06-06", 1), ("06-01", 1), ("06-05", 1), ("06-48", 1), ("06-47", 1)
+]
+my_dir = "/Users/brooke/Desktop/Small Jobs/"
+beginning_PDF_file = "scan00650.pdf"
 
-# job number: number of PDFs for said job
-my_dict = {
-    7300: 1, 7302: 3, 7305: 1, 7306: 1, 7307: 1, 7308: 1, 7309: 2, 7310: 19,
-    7311: 2, 7315: 1, 7317: 1, 7312: 1, 7313: 4, 7314: 1, 7316: 1, 7318: 1,
-    7319: 1, 7321: 3, 7322: 1, 7323: 3, 7324: 2, 7325: 1, 7326: 1, 7327: 1,
-    7328: 1, 7329: 7, 7330: 2, 7331: 3, 7332: 1, 7333: 1, 7334: 1, 7336: 1,
-    7337: 3, 7338: 2, 7340: 1, 7341: 2, 7342: 1, 7343: 3, 7344: 1, 7345: 1,
-    7346: 1, 7348: 1, 7351: 1, 7352: 2, 7353: 1, 7354: 2, 7355: 1, 7356: 1,
-    7357: 1, 7358: 1, 7359: 2, 7363: 1, 7364: 3, 7365: 1, 7366: 1, 7367: 1,
-    7368: 5, 7369: 1, 7370: 25, 7371: 1, 7372: 3, 7373: 3, 7374: 1, 7375: 1,
-    7376: 1, 7377: 4, 7378: 2, 7379: 1, 7381: 1, 7382: 1, 7383: 2, 7384: 6,
-    7385: 1, 7388: 1, 7389: 1, 7390: 1, 7391: 5, 7393: 2, 7394: 3, 7395: 2,
-    7396: 1, 7397: 4, 7398: 2, 7399: 1
-}
-# directory in which all PDFs are stored
-my_dir = "/Users/brooke/Desktop/jobs/"
-# the filename of the first PDF in my_dir
-beginning_pdf_file = "scan00906.pdf"
-
-ex = OrganizePDFs(directory=my_dir, beginning_file=beginning_pdf_file,
-                  job_dict=my_dict)
+ex = OrganizePDFs(directory=my_dir, beginning_file=beginning_PDF_file,
+                  job_list=cur_job_list)
 ex.organize()
